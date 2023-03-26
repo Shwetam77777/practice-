5a3ed52645ea1636f2e272ab4535ac4a235c85da
Shweta mishra <111523337+Shwetam77777@users.noreply.github.com>
2023-03-26 21:44:14 +0530

    Add files via upload

---
+++ DataScienceEcosystem.py
@@ -0,0 +1,94 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Data Science Tools and Ecosystem
+
+# ## Data Science Tools and Ecosystem are summarized
+
+# A data ecosystem refers to a combination of enterprise infrastructure and applications that is utilized to aggregate and analyze information. It enables organizations to better understand their customers and craft superior marketing, pricing and operations strategies.
+# 
+
+# # Some of the popular languages that Data Scientists use are:
+
+# 1. Python.
+# 2. SQL
+# 3. R
+
+# # Some of the commonly used libraries used by Data Scientists include:
+
+# 1.pandas 
+# 
+# 2.sciket learn
+# 
+# 3.Matplot
+# 
+# 
+
+# # Data Science Tools
+
+# 1.IDE
+# 
+# 2.git
+# 
+# 3.R studio
+
+# # arithmetic expressions in Python
+
+# In[8]:
+
+
+a=5
+b=7
+c=a+b
+s=b-a
+d=a/c
+m=a*b
+print(c)
+print(s)
+print(m)
+print(d)
+
+
+# # multiply and add numbers
+
+# In[9]:
+
+
+(3*4)+5
+
+
+# # This will convert 200 minutes to hours by diving by 60
+
+# In[14]:
+
+
+200/60
+
+
+# # Objectives
+
+# Some of the popular languages that Data Scientists use are:
+# Python.
+# SQL
+# R
+# Some of the commonly used libraries used by Data Scientists include:
+# 1.pandas
+# 
+# 2.sciket learn
+# 
+# 3.Matplot
+# 
+# Data Science Tools
+# 1.IDE
+# 
+# 2.git
+# 
+# 3.R studio
+
+# # Author's name : Shweta Mishra
+
+# In[ ]:
+
+
+
+
